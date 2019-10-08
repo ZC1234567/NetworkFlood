431bb6848bcc438b5f9810b26531fea6e99d99da
ZC1234567 <15905188618@163.com>
2019-10-08 11:07:14 +0800

    [andy][add][20191008]add all

---
+++ hdr/CfgId.h
@@ -0,0 +1,43 @@
+#ifndef _CFGID_H_
+#define _CFGID_H_
+
+#include <iostream> 
+
+#define CFG_MAX_DEPTH 5 
+
+namespace NS_CONFIG
+{
+	typedef enum CFGID
+	{
+		EM_CFGID_START = -1,                //枚举保护
+		EM_CFGID_SERVER_IP = 0,             //服务端ip
+		EM_CFGID_SERVER_PORT,               //服务端port
+		EM_CFGID_QUEUE_MAX_SIZE,            //缓冲区最大长度
+		EM_CFGID_THREAD_NUM,                //任务中心启动线程数量
+		EM_CFGID_END                        //枚举保护
+	}EM_CFGID;
+	
+	typedef struct CfgInfo
+	{
+		NS_CONFIG::EM_CFGID CfgId;                      //配置id
+		const char* filename;                           //配置文件名
+		const char* cfgpath[CFG_MAX_DEPTH];             //配置路径 
+	}CfgInfo;
+}
+
+//配置信息数组
+static const NS_CONFIG::CfgInfo g_stCfgInfo[] = {
+	//Processor
+	//服务端ip
+	{NS_CONFIG::EM_CFGID_SERVER_IP, "../etc/flood.json", {"PROCESSOR", "SERVER_IP", (const char *)0}},
+	//服务端port
+	{NS_CONFIG::EM_CFGID_SERVER_PORT, "../etc/flood.json", {"PROCESSOR", "SERVER_PORT", (const char *)0}},
+	//缓冲区最大长度
+	{NS_CONFIG::EM_CFGID_QUEUE_MAX_SIZE, "../etc/flood.json", {"PROCESSOR", "QUEUE_MAX_SIZE", (const char *)0}},
+	
+	//TaskMgr
+	//线程启动数量
+	{NS_CONFIG::EM_CFGID_THREAD_NUM, "../etc/flood.json", {"TASKMGR", "THREAD_NUM", (const char*)0}}
+};
+
+#endif 
